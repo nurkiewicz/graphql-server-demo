e4f61647de80008dc4a94204c21d88ae996a9336
Tomasz Nurkiewicz <nurkiewicz@gmail.com>
2019-08-21 00:41:37 +0200

    Adding Zipkin for better observability

--- src/main/java/com/nurkiewicz/graphql/GraphqlApplication.java
+++ src/main/java/com/nurkiewicz/graphql/GraphqlApplication.java
@@ -1,9 +1,13 @@
 package com.nurkiewicz.graphql;
 
+import brave.sampler.Sampler;
 import com.devskiller.jfairy.Fairy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import zipkin2.reporter.AsyncReporter;
+import zipkin2.reporter.Reporter;
+import zipkin2.reporter.okhttp3.OkHttpSender;
 
 @SpringBootApplication
 public class GraphqlApplication {
@@ -15,6 +19,16 @@ public class GraphqlApplication {
 	@Bean
 	Fairy fairy() {
 		return Fairy.create();
+	}
+
+	@Bean
+	Reporter reporter() {
+		return AsyncReporter.create(OkHttpSender.create("http://localhost:9411/api/v2/spans"));
+	}
+
+	@Bean
+	public Sampler alwaysSampler() {
+		return Sampler.ALWAYS_SAMPLE;
 	}
 
 }
--- src/main/java/com/nurkiewicz/graphql/PlayerResolver.java
+++ src/main/java/com/nurkiewicz/graphql/PlayerResolver.java
@@ -1,9 +1,14 @@
 package com.nurkiewicz.graphql;
 
+import brave.ScopedSpan;
+import brave.Tracer;
 import com.coxautodev.graphql.tools.GraphQLResolver;
+import com.google.common.base.Throwables;
 import com.google.common.collect.ImmutableList;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.Callable;
 
 @Component
 @RequiredArgsConstructor
@@ -13,22 +18,43 @@ class PlayerResolver implements GraphQLResolver<Player> {
     private final InventoryClient inventoryClient;
     private final PlayerMetadata playerMetadata;
     private final PointsCalculator pointsCalculator;
+    private final Tracer tracer;
 
-    Billing billing(Player player) {
-        return billingRepository.forUser(player.getId());
+    Billing billing(Player player) throws InterruptedException {
+        return inSpan("billing",
+                () -> billingRepository.forUser(player.getId())
+        );
     }
 
-    String name(Player player) {
-        return playerMetadata.lookupName(player.getId());
+    String name(Player player) throws InterruptedException {
+        return inSpan("name",
+                () -> playerMetadata.lookupName(player.getId())
+        );
     }
 
-    int points(Player player) {
-        return pointsCalculator.pointsOf(player.getId());
+    int points(Player player) throws InterruptedException {
+        return inSpan("points",
+                () -> pointsCalculator.pointsOf(player.getId())
+        );
     }
 
-    ImmutableList<Item> inventory(Player player) {
-        return inventoryClient.loadInventory(player.getId());
+    ImmutableList<Item> inventory(Player player) throws InterruptedException {
+        return inSpan("inventory",
+                () -> inventoryClient.loadInventory(player.getId())
+        );
     }
 
+    private <T> T inSpan(String name, Callable<T> action) {
+        ScopedSpan span = tracer.startScopedSpan(name);
+        try {
+            return action.call();
+        } catch (Exception e) {
+            span.error(e);
+            Throwables.throwIfUnchecked(e);
+            throw new RuntimeException(e);
+        } finally {
+            span.finish();
+        }
+    }
 
 }
